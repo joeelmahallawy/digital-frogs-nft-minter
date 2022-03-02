b920bb03a19f7aa2928fccad343db70d90209cbf
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-03-02 16:29:45 -0500

    deployed polygon contract

--- utils/index.ts
+++ utils/index.ts
@@ -3,4 +3,5 @@ import Web3 from "web3";
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 const TOTAL_NUMBER_OF_NFTS = 500;
 const POLYGON_MUMBAI_CHAIN_ID = 137;
+
 export { web3, TOTAL_NUMBER_OF_NFTS, POLYGON_MUMBAI_CHAIN_ID };
--- web3/contractMethods/mint.ts
+++ web3/contractMethods/mint.ts
@@ -1,7 +1,17 @@
+import { web3 } from "../../utils";
+
 const mintNFT = async (setTokenid, state, toast, onOpen, setshowModal) => {
+  const gasLimit = await web3.eth.getBlock("latest");
   return state.value?.contract.methods
     .safeMint(state.value?.wallet)
-    .send({ from: state.value?.wallet, gasLimit: 500000000000000 })
+    .send({
+      from: state.value?.wallet,
+      // gasLimit: 500000000000000,
+      // gasPrice: 92000000000,
+      gasPrice: web3.eth.getGasPrice(),
+      gasLimit: gasLimit.gasLimit,
+      gas: 5000000,
+    })
     .then((val) => {
       const tokenID = val.events.Transfer.returnValues.tokenId;
       setTokenid(tokenID);
